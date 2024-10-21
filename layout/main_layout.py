ee0385e7c1862dfab65f67b75fc378c005105e53
Rodrigo Oliveira <roliveira.5609@unimar.edu.ve>
2024-10-20 23:09:20 -0400

    fix: Components, Layouts, Tailwind. Add: .gitinore and .editorconfig

--- layout/main_layout.py
+++ layout/main_layout.py
@@ -1,5 +1,5 @@
 from fasthtml.common import *
-from layout.mm1 import calculation_button
+from component.buttons import calculation_button
 
 def main_layout():
     return Div(
@@ -11,21 +11,21 @@ def main_layout():
             # Fila de cálculos simples
             Div(
                 Div("SIMPLE", cls="bg-red-600 text-white text-center py-2 rounded-t-md font-bold"),
-                Div(
-                    calculation_button("ℹ️", "M/M/1", "Población infinita canal simple", "bg-white", "text-black", "/calcularmm1"),
-                    calculation_button("ℹ️", "M/M/1/M/M", "Población finita canal simple", "bg-white", "text-black", "#"),
-                    cls="grid grid-cols-2 gap-4"
-                ),
+                    Div(
+                        calculation_button("ℹ️", "M/M/1", "Población infinita canal simple", "bg-white", "text-black", "/calcularmm1"),
+                        calculation_button("ℹ️", "M/M/1/M/M", "Población finita canal simple", "bg-white", "text-black", "#"),
+                        cls="grid grid-cols-2 gap-4"
+                    ),
                 cls="rounded-md overflow-hidden"
             ),
             # Fila de cálculos múltiples
             Div(
                 Div("MULTIPLE", cls="bg-yellow-500 text-black text-center py-2 rounded-t-md font-bold"),
-                Div(
-                    calculation_button("⚠️", "EN DESAROLLO", "Próximamente", "bg-white", "text-black", "#"),
-                    calculation_button("⚠️", "EN DESAROLLO", "Próximamente", "bg-white", "text-black", "#"),
-                    cls="grid grid-cols-2 gap-4"
-                ),
+                    Div(
+                        calculation_button("⚠️", "EN DESAROLLO", "Próximamente", "bg-white", "text-black", "#"),
+                        calculation_button("⚠️", "EN DESAROLLO", "Próximamente", "bg-white", "text-black", "#"),
+                        cls="grid grid-cols-2 gap-4"
+                    ),
                 cls="rounded-md overflow-hidden mt-4"
             ),
             cls="space-y-4 mt-6"
---
+++ main.py
@@ -0,0 +1,102 @@
+from fasthtml import FastHTML
+from fasthtml.common import *
+from component.header import header
+from component.footer import footer
+from layout.main_layout import main_layout
+
+app, rt = fast_app()
+
+@rt("/")
+def index():
+    # Vista Principal: Muestra las opciones que se pueden calcular
+    return Html(
+        Head(
+            Link(rel="stylesheet", href="/static/output.css"),
+            Title("Inicio")
+        ),
+        Body(
+            Div(
+                header(),
+                    Div(
+                        main_layout(),
+                        cls="container mx-auto p-6 flex-grow"
+                    ), 
+                footer(),
+                cls="flex flex-col min-h-screen"  
+            ),
+            cls="m-0"
+        ),      
+    )
+
+@rt("/calcularmm1", methods=["GET"])
+def calcular_cola():
+    # Vista Calculadora M/M/1: Permite introducir las variables para calcular M/M/1
+    form = Div(
+        Form(
+            H2("M/M/1", cls="font-medium text-black text-center"),
+            Label("Tasa de llegada: ", Input(name="tasa_llegada", type="number", cls="flex rounded-md border mt-2 px-3 py-1"), cls="block font-medium text-black", requires),
+            Label("Tasa de servicio: ", Input(name="tasa_servicio", type="number", cls="flex rounded-md border mt-2 px-3 py-1"), cls="block font-medium text-black"),
+            Label("Servidores: ", Input(name="servidores", type="number", cls="flex rounded-md border mt-2 px-3 py-1"), cls="block font-medium text-black"),
+            Button("Calcular", type="submit", cls="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"),
+            action = '/result_MM1',
+            method="POST",
+            cls= "space-y-6"
+        ),
+        cls="p-6 bg-white border border-gray-200 rounded-lg shadow"
+    )
+    
+    return Html(
+        Head(
+            Link(rel="stylesheet", href="/static/output.css"),
+            Title("Calculadora de Teoria de Colas")
+        ),
+        Body(
+            Div(
+                header(),
+                    Div(
+                        form,
+                        cls="container flex justify-center mx-auto p-6 flex-grow"
+                    ), 
+            footer(),
+            cls="flex flex-col min-h-screen"  
+            ),
+            cls="m-0"
+        ),      
+    )
+
+@rt("/result_MM1", methods=["POST"])
+def resultado(tasa_llegada : float, tasa_servicio : float, servidores : int):
+    
+    # Cálculos de teoría de colas
+    rho = tasa_llegada / (servidores * tasa_servicio)
+    vacio = (1 - rho)
+    
+    resultado = Div(
+        Div(f"Factor de utilización (rho): {rho:.2f}", cls="text-lg text-green-500"), 
+        H2(f"Factor que el servidor este vacio P0: {vacio:.2f}", cls="text-lg text-green-500"),
+        Div(
+            A('Volver', href='/', cls="text-red-300")
+        )
+    )
+    
+    return Html(
+        Head(
+            Link(rel="stylesheet", href="/static/output.css"),
+            Title("Calculadora de Teoria de Colas")
+        ),
+        Body(
+            Div(
+                header(),
+                    Div(
+                        resultado,
+                        cls="container mx-auto p-6 flex-grow"
+                    ), 
+            footer(),
+            cls="flex flex-col min-h-screen"  
+            ),
+            cls="m-0"
+        ),      
+    )
+    
+
+serve()
