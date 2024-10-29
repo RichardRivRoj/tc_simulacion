47bce7a7683fe18283fe5c63f04275a85297ad7f
Richard Rivera <rojasrichard0603@outlook.com>
2024-10-28 22:53:43 -0400

    Ni idea

--- views/calc_mms.py
+++ views/calc_mms.py
@@ -6,9 +6,9 @@ def calc_mms():
     # Vista Calculadora M/M/1: Permite introducir las variables para calcular M/M/1
     form = card(
         Form(
-            Label("λ Tasa de llegada: ", Input(name="v_lambda", type="number", cls="flex rounded-md border mt-4 p-2", min=0.0001, required=True, hx_validate=True), cls="block font-medium text-black text-lg"),
-            Label("μ Tasa de servicio: ", Input(name="v_mu", type="number", cls="flex rounded-md border mt-4 p-2", min=0.0001, required=True, hx_validate=True), cls="block font-medium text-black text-lg"),
-            Label("c Número de servidores: ", Input(name="v_c", type="number", cls="flex rounded-md border mt-4 p-2", min=0.0001, required=True, hx_validate=True), cls="block font-medium text-black text-lg"),
+            Label("λ Tasa de llegada: ", Input(name="v_lambda", type="number", cls="flex rounded-md border mt-4 p-2", required=True, hx_validate=True), cls="block font-medium text-black text-lg"),
+            Label("μ Tasa de servicio: ", Input(name="v_mu", type="number", cls="flex rounded-md border mt-4 p-2", required=True, hx_validate=True), cls="block font-medium text-black text-lg"),
+            Label("c Número de servidores: ", Input(name="v_c", type="number", cls="flex rounded-md border mt-4 p-2", required=True, hx_validate=True), cls="block font-medium text-black text-lg"),
             Button("Calcular", type="submit", hx_post='/result_mms', hx_target='#response', cls="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg px-5 py-2.5 text-center"),
             action = '/result_mms',
             method="POST",
